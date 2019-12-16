bee58cb64d61deea22f1facd95f1a703b4de5090
zhzalkar <zhzalkar@outlook.com>
2019-12-16 16:38:07 +0600

    roll dice functionality added

--- app.js
+++ app.js
@@ -11,9 +11,28 @@ GAME RULES:
 
 var score = [0, 0],
     roundScore = 0,
-    activePlayer = 0,
-    dice = Math.floor(Math.random() * 6) + 1;
+    activePlayer = 0;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// Set initial values to 0
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+
+    // Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // Update the round score IF the rolled number was NOT 1
+
+
+});
